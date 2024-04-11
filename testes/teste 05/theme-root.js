c0a1fbff04ffbfa8a76ef601030463960c8838d7
Gustavo Soares Lima Rios <gustavo.rios801208@gmail.com>
2024-04-11 11:55:25 -0300

    Update

---
+++ testes/teste 05/theme-root.js
@@ -0,0 +1,51 @@
+window.onload = function() {
+    getThemeFromLocalStorage()
+  }
+  
+  const rootElement = document.documentElement
+  const inputContainer = document.querySelector('.theme-on-off')
+  
+  const lightTheme = {
+    '--background-color': '#F5F5F5',
+    '--text-color': '#000',
+  }
+  
+  const darkTheme = document.getElementById('darkmode:root')
+  
+  inputContainer.addEventListener('change', function() {
+    const isChecked = inputContainer.checked
+    if (isChecked) {
+        changeTheme(darkTheme)
+    } else {
+        changeTheme(lightTheme)
+    }
+  })
+  
+  function changeTheme(theme) {
+    for (let prop in theme) {
+        changeProperty(prop, theme[prop])
+    }
+    saveThemeToLocalStorage(theme)
+  }
+  
+  function changeProperty(property, value) {
+    rootElement.style.setProperty(property, value)
+  }
+  
+  function saveThemeToLocalStorage(theme) {
+    localStorage.setItem('theme', JSON.stringify(theme))
+  }
+  
+  function getThemeFromLocalStorage() {
+    const theme = JSON.parse(localStorage.getItem('theme'))
+    changeTheme(theme)
+    if (isThemeEqual(theme, darkTheme)) inputContainer.checked = true
+  }
+  
+  function isThemeEqual(firstTheme, secondTheme) {
+    for (let prop in firstTheme) {
+        if (firstTheme[prop] !== secondTheme[prop]) return false
+    }
+    return true
+  }
+  
